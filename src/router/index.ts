68973205f5d340556e1482a7a5547f0c848f2efb
ADMIN-PC\Admin <tuantuhoang1999@gmail.com>
2026-07-09 17:45:57 +0700

    config router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,26 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+} from "vue-router";
+import Intro from "@/views/intro/Intro.vue";
 import Login from "@/views/login/Login.vue";
 import ForgotPassword from "@/views/login/ForgotPassword.vue";
 import Register from "@/views/login/Register.vue";
 import ResetPassword from "@/views/login/ResetPassword.vue";
 
-const routes = [
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    redirect: "/intro",
+    children: [
+      {
+        path: "/intro",
+        name: "Intro",
+        component: Intro,
+      },
+    ],
+  },
   {
     path: "/login",
     name: "Login",
@@ -32,14 +48,20 @@ const router = createRouter({
   routes: routes,
 });
 
+const protectedPaths = ["/reset-password"];
+
 router.beforeEach((to, from, next) => {
   const token = localStorage.getItem("access_token");
+  const requiresAuth =
+    protectedPaths.includes(to.path) ||
+    to.matched.some((record) => record.meta?.requiresAuth);
 
-  if (to.meta.requiresAuth && !token) {
-    next("/login");
-  } else {
-    next();
+  if (requiresAuth && !token) {
+    next({ path: "/login", query: { redirect: to.fullPath } });
+    return;
   }
+
+  next();
 });
 
 export default router;
